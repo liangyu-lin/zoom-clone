5cb201be25225f0aeb260d341cffa835b72fb196
LiangYu <liangyulin0727@gmail.com>
2021-02-15 14:44:16 -0800

    added video stream, still fixing on bugs

---
+++ public/script.js
@@ -0,0 +1,23 @@
+const videoGrid = document.getElementById('video-grid')
+const myVideo = document.createElement('video')
+myVideo.muted = true
+console.log(videoGrid)
+let myVideoStream;
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true,
+
+}).then(stream => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+})
+
+
+const addVideoStream = (video, stream) => {
+    video.srcObject = stream;
+    video.addEventListener('loadmetadata', () => {
+        video.play()
+    })
+    videoGrid.append(video);
+}
